e3c78cd7adeaea4b03e0c655ba1f3bb0dde3fdcc
Braine Lomoni <kapolonbraine@gmail.com>
2023-08-05 11:40:15 +0300

    refactor : Log out database name to console.

--- express-app.js
+++ express-app.js
@@ -18,19 +18,20 @@ app.use(bodyParser.json());
 //Database variables
 const databaseName = "san-code.db"
 const tableName = "sanCodeStudent" //Student records table
-const staffTableName = "sanCodeStaff"
-const reportTableName = "sanCodeReport"
+const staffTableName = "sanCodeStaff" //Staff records table
+const reportTableName = "sanCodeReport" //Report table
 // Timestamp
 const timestamp = moment().tz('Africa/Nairobi').format('YYYY-MM-DD HH:mm:ss');
 // Start of today date
 const startOfToday = moment().startOf("day");
+//End of today date
 const endOfToday = moment().endOf("day");
 
 const db = new sqlite3.Database(`database/${databaseName}`, (err) => {
     if (err) {
         console.error(err.message);
     } else {
-        console.log('Connected to the database.');
+        console.log(`Connected to the database: ${databaseName}`);
     }
 });
 
